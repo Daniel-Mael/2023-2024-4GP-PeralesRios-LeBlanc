a417dd924436a4d5c9da2d39f2712e58a087335b
Daniel PERALES RIOS <m_leblan@insa-toulouse.fr>
2024-03-24 13:34:10 +0100

    Update Code_complet.ino
    Change of some details and creation of a dedicated function for the potentiometer reading

--- Code_complet/Code_complet.ino
+++ Code_complet/Code_complet.ino
@@ -14,11 +14,13 @@ Description of the project:
     the .aia file to the smartphone via a QR code.
 */
 
-
+// Definition of the libraries used in the code
 #include <Adafruit_SSD1306.h>
+#include <SPI.h>
 
 // Parameter definition
 /////////////////////////////////////////////////////////////////////////
+
 #define encoderPinA 2 // CLK Output A
 #define encoderPinB 4 // DT Output B
 #define Switch 5 // Switch connection
@@ -26,20 +28,20 @@ Description of the project:
 #define nombreDePixelsEnHauteur 64
 #define brocheResetOLED -1
 #define adresseI2CecranOLED 0x3C
-
 volatile long encoderValue = 0;
 long lastEncoderValue = 0;
-
 long lastDebounceTime = 0;
 long debounceDelay = 120;
-bool enterMenu = false; 
-
-
+bool action = false; 
 Adafruit_SSD1306 ecranOLED(nombreDePixelsEnLargeur, nombreDePixelsEnHauteur, &Wire, brocheResetOLED);
-
 String menuItems[] = {"Potentiometer", "Flex Sensor", "Graphite Sensor"}; // Definition of menu items
-int selectedItem = 0; // Menu items are defined from 0 to 2
+String potentiometerItems[] = {"Potentiometer reading", "Current value:"}; // Definition of items in the potentiometer screen
+int selectedItem = 0; // Menu items are defined from 0 to x (depending on the menu)
+long valuePot = 0; // Potentiometer value; 
+
 /////////////////////////////////////////////////////////////////////////
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -53,6 +55,8 @@ void setup() {
   ecranOLED.setTextColor(SSD1306_WHITE);
 }
 
+
+
 void loop() {
   displayMenu();
   handleMenuItemSelection(selectedItem);
@@ -61,12 +65,14 @@ void loop() {
   if (encoderValue != lastEncoderValue) { // If we turn the encoder, then the selectedItem variable is increased
     selectedItem++;
     if (selectedItem == 3) {
-      selectedItem = 1; // If we reach the last element from the menu, the selectedItem is reset to the first element
+      selectedItem = 0; // If we reach the last element from the menu, the selectedItem is reset to the first element
     }
   }  
   Serial.println("Selected:" + String(selectedItem)); // To debug
   delay(150); // We add a delay between loops for stability, while still having a responsive display
 }
+
+
 
 void displayMenu() {
   ecranOLED.clearDisplay();
@@ -83,25 +89,28 @@ void displayMenu() {
 }
 
 
+
 void handleMenuItemSelection(int selectedItem) {
   // Implement actions for each menu item
-  switch (selectedItem) {
-    case 1:
-      if (enterMenu) { // For each case, if the switch button was pressed, we enter each menu respectively
-      
+  switch (selectedItem) { // For each case, if the switch button was pressed, we enter each menu respectively
+    case 0: // Potentiometer setting using a dedicated function
+      if (action) {
+        displayPotentiometer(valuePot);
       }
-    case 2:
-      if (enterMenu) {
+    case 1: // Flex sensor reading and display using a dedicated function
+      if (action) {
         
       }
-    case 3:
-      if (enterMenu) {
+    case 2: // Graphite sensor reading and display using a dedicated function
+      if (action) {
         
       }
     default:
       break;
   }
 }
+
+
 
 void updateEncoder() {
   if (encoderPinB == HIGH) {
@@ -112,6 +121,8 @@ void updateEncoder() {
   }
 }
 
+
+
 void switchButton() {
   // Debouncing logic for the switch button
   long currentTime = millis();
@@ -120,7 +131,24 @@ void switchButton() {
     currentTime = millis(); // The function waits for the debounce delay to pass, and then we read the value of the switch again
   }
   if (Switch == HIGH) { // If, after the debounce delay, the Switch is still high, we have effectively pressed the button
-    enterMenu = true; // We enter the menu we were currently in
+    action = true; // We choose the selected option
   }
+}
+
+
+
+void displayPotentiometer(int valuePot) {
+    selectedItem = 0; 
+    ecranOLED.clearDisplay();
+    for (int i = 0; i < sizeof(potentiometerItems) / sizeof(potentiometerItems[0]); i++) {
+        if (i == selectedItem) {
+        ecranOLED.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Highlight selected item
+        } else {
+        ecranOLED.setTextColor(SSD1306_WHITE); // Regular color for other items
+        }
+        ecranOLED.setCursor(0, i * 10); // Adjust position for each item
+        ecranOLED.print(menuItems[i]); // Print each element
+    }
+    ecranOLED.display();
 }
 
