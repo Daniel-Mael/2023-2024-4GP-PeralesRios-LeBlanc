07d56c1be84885e6b7464d390f042b6e25322d10
Daniel PERALES RIOS <m_leblan@insa-toulouse.fr>
2024-03-24 13:56:18 +0100

    Update Code_complet.ino
    Update for the flex sensor function

--- Code_complet/Code_complet.ino
+++ Code_complet/Code_complet.ino
@@ -44,42 +44,57 @@ long lastDebounceTime = 0;
 long debounceDelay = 120;
 long valuePot = 0; // Potentiometer value; 
 bool action = false; // This variable becomes true whenever the switch button is pressed, false otherwise
+bool MainMenu = false; // This bool is true when we are in the main menu, false otherwise. Useful to avoid bugs
 String menuItems[] = {"Potentiometer", "Flex Sensor", "Graphite Sensor"}; // Definition of menu items
-String potentiometerItems[] = {"Potentiometer reading", "Current value:"}; // Definition of items in the potentiometer screen
-String flexItems[] = {"Flex sensor reading", "Current value:"}; // Definition of items in the flex sensor screen
+String potentiometerItems[] = {"Potentiometer reading", "Current value:", "BACK"}; // Definition of items in the potentiometer screen
+String flexItems[] = {"Flex sensor reading", "Resistance value:", "", "Bend angle:", "", "BACK"}; // Definition of items in the flex sensor screen
 int selectedItem = 0; // Menu items are defined from 0 to x (depending on the menu)
+float Rflex = 0; 
+float angle = 0; 
 
 /////////////////////////////////////////////////////////////////////////
 
 
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(encoderPinA, INPUT_PULLUP); // Use internal pull-up resistors for the encoder
-  pinMode(encoderPinB, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(encoderPinB), updateEncoder, RISING); // Whenever the encoderPinB changes state, an interruption occurs.
-  attachInterrupt(digitalPinToInterrupt(Switch), switchButton, RISING); // Whenever the switch button is pressed, an interruption is raised
-  if (!ecranOLED.begin(SSD1306_SWITCHCAPVCC, adresseI2CecranOLED))
-    while (1); // Program stops (infinite loop) if something goes wrong here
-  ecranOLED.clearDisplay();
-  ecranOLED.setTextColor(SSD1306_WHITE);
+    Serial.begin(9600);
+    pinMode(encoderPinA, INPUT_PULLUP); // Use internal pull-up resistors for the encoder
+    pinMode(encoderPinB, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(encoderPinB), updateEncoder, RISING); // Whenever the encoderPinB changes state, an interruption occurs.
+    attachInterrupt(digitalPinToInterrupt(Switch), switchButton, RISING); // Whenever the switch button is pressed, an interruption is raised
+    
+    if (!ecranOLED.begin(SSD1306_SWITCHCAPVCC, adresseI2CecranOLED))
+        while (1); // Program stops (infinite loop) if something goes wrong here
+        
+    pinMode(flexPin, INPUT);
+    ecranOLED.clearDisplay();
+    ecranOLED.setTextColor(SSD1306_WHITE);
 }
 
 
 
 void loop() {
-  displayMenu();
-  handleMenuItemSelection(selectedItem);
-  encoderValue = abs(encoderValue % 3); 
-  Serial.println(encoderValue); // To debug
-  if (encoderValue != lastEncoderValue) { // If we turn the encoder, then the selectedItem variable is increased
-    selectedItem++;
-    if (selectedItem == 3) {
-      selectedItem = 0; // If we reach the last element from the menu, the selectedItem is reset to the first element
+    if (MainMenu) {
+        displayMenu();
     }
-  }  
-  Serial.println("Selected:" + String(selectedItem)); // To debug
-  delay(150); // We add a delay between loops for stability, while still having a responsive display
+    else {
+        handleMenuItemSelection(selectedItem);
+    }
+    encoderValue = abs(encoderValue % 3); 
+    Serial.println(encoderValue); // To debug
+    if (encoderValue != lastEncoderValue) { // If we turn the encoder, then the selectedItem variable is increased
+        selectedItem++;
+        if (selectedItem == 3) {
+        selectedItem = 0; // If we reach the last element from the menu, the selectedItem is reset to the first element
+        }
+    }  
+    Serial.println("Selected:" + String(selectedItem)); // To debug
+    int ADCflex = analogRead(flexPin); // Flex sensor: read the ADC, and calculate voltage and resistance from it
+    float Vflex = (ADCflex / 1024.0) * VCC;
+    Rflex = R_DIV * (VCC / Vflex - 1.0); // Flex sensor: we calculate the approximate resistance value
+    angle = map(Rflex, flatResistance, bendResistance, 0, 90.0);  // Flex sensor: use the calculated resistance to estimate the 
+                                                                        // sensor's bend angle
+    delay(200); // We add a delay between loops for stability, while still having a responsive display
 }
 
 
@@ -148,7 +163,6 @@ void switchButton() {
 
 
 void displayPotentiometer(int valuePot) {
-    selectedItem = 0; 
     ecranOLED.clearDisplay();
     for (int i = 0; i < sizeof(potentiometerItems) / sizeof(potentiometerItems[0]); i++) {
         if (i == selectedItem) {
@@ -165,16 +179,13 @@ void displayPotentiometer(int valuePot) {
 
 
 void displayFlexSensor(int valueFlex) {
-    selectedItem = 0; 
     ecranOLED.clearDisplay();
+    flexItems[2] = Rflex; // Update the values of the resistance and the angle of the flex sensor
+    flexItems[4] = angle; 
     for (int i = 0; i < sizeof(flexItems) / sizeof(flexItems[0]); i++) {
-        if (i == selectedItem) {
-        ecranOLED.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Highlight selected item
-        } else {
         ecranOLED.setTextColor(SSD1306_WHITE); // Regular color for other items
-        }
         ecranOLED.setCursor(0, i * 10); // Adjust position for each item
-        ecranOLED.print(potentiometerItems[i]); // Print each element
+        ecranOLED.print(flexItems[i]); // Print each element
     }
     ecranOLED.display();
 }
