833699a23a45ca48223b65068269074ca6f0e46d
Daniel PERALES RIOS <m_leblan@insa-toulouse.fr>
2024-03-24 14:30:40 +0100

    Update Code_complet.ino
    Update of the dedicated functions and teh rotary encoder one

--- Code_complet/Code_complet.ino
+++ Code_complet/Code_complet.ino
@@ -44,7 +44,7 @@ long lastDebounceTime = 0;
 long debounceDelay = 120;
 long valuePot = 0; // Potentiometer value; 
 bool action = false; // This variable becomes true whenever the switch button is pressed, false otherwise
-bool MainMenu = false; // This bool is true when we are in the main menu, false otherwise. Useful to avoid bugs
+bool MainMenu = true; // This bool is true when we are in the main menu, false otherwise. Useful to avoid bugs
 String menuItems[] = {"Potentiometer", "Flex Sensor", "Graphite Sensor"}; // Definition of menu items
 String potentiometerItems[] = {"Potentiometer reading", "Current value:", "BACK"}; // Definition of items in the potentiometer screen
 String flexItems[] = {"Flex sensor reading", "Resistance value:", "", "Bend angle:", "", "BACK"}; // Definition of items in the flex sensor screen
@@ -76,19 +76,12 @@ void setup() {
 void loop() {
     if (MainMenu) {
         displayMenu();
+        updateEncoder((sizeof(menuItems) / sizeof(menuItems[0])));  // The number of items of a list is defined as the number of bytes of the
+                                                                    // whole list divided by the bytes occupied by one element
     }
     else {
         handleMenuItemSelection(selectedItem);
     }
-    encoderValue = abs(encoderValue % 3); 
-    Serial.println(encoderValue); // To debug
-    if (encoderValue != lastEncoderValue) { // If we turn the encoder, then the selectedItem variable is increased
-        selectedItem++;
-        if (selectedItem == 3) {
-        selectedItem = 0; // If we reach the last element from the menu, the selectedItem is reset to the first element
-        }
-    }  
-    Serial.println("Selected:" + String(selectedItem)); // To debug
     int ADCflex = analogRead(flexPin); // Flex sensor: read the ADC, and calculate voltage and resistance from it
     float Vflex = (ADCflex / 1024.0) * VCC;
     Rflex = R_DIV * (VCC / Vflex - 1.0); // Flex sensor: we calculate the approximate resistance value
@@ -121,10 +114,12 @@ void handleMenuItemSelection(int selectedItem) {
     case 0: // Potentiometer setting using a dedicated function
       if (action) {
         displayPotentiometer(valuePot);
+        updateEncoder(sizeof(potentiometerItems) / sizeof(potentiometerItems[0])); 
       }
     case 1: // Flex sensor reading and display using a dedicated function
       if (action) {
         displayFlexSensor(valueFlex); 
+        updateEncoder(sizeof(flexItems) / sizeof(flexItems[0])); 
       }
     case 2: // Graphite sensor reading and display using a dedicated function
       if (action) {
@@ -137,19 +132,37 @@ void handleMenuItemSelection(int selectedItem) {
 
 
 
-void updateEncoder() {
-  if (encoderPinB == HIGH) {
-    encoderValue++;
-  }
-  else {
-    encoderValue--;
-  }
+void updateEncoder(int NumberOfItems) {
+    /*
+    This function allows to control the menu when the rotary encoder is turned. This function takes as input the 
+    number of elements of each menu to modify the last element than can be selected accordingly.
+    */
+
+    if (encoderPinB == HIGH) {
+        encoderValue++;
+    }
+    else {
+        encoderValue--;
+    }
+    encoderValue = abs(encoderValue % 3); 
+    Serial.println(encoderValue); // To debug
+    if (encoderValue != lastEncoderValue) { // If we turn the encoder, then the selectedItem variable is increased
+        selectedItem++;
+        if (selectedItem == NumberOfItems) {
+            selectedItem = 0; // If we reach the last element from the menu, the selectedItem is reset to the first element
+        }  
+        Serial.println("Selected:" + String(selectedItem)); // To debug
+    }
+    lastEncoderValue = encoderValue; // The last value from the encoder is updated
 }
 
 
 
 void switchButton() {
-  // Debouncing logic for the switch button
+  /*
+  This function controls the reading of the switch button. It has debouncing logic to minimize bad readings.
+  */
+
   long currentTime = millis();
   lastDebounceTime = currentTime;
   while (currentTime - lastDebounceTime < debounceDelay) {
@@ -157,22 +170,31 @@ void switchButton() {
   }
   if (Switch == HIGH) { // If, after the debounce delay, the Switch is still high, we have effectively pressed the button
     action = true; // We choose the selected option
+    if (MainMenu) {
+        MainMenu = false; // If we were in the main menu, we set this variable to false so then the option and not the menu is displayed
+        selectedItem = 0; // We reset selectedItem to 0 so we can control the different menus with this same variable
+    }
   }
 }
 
 
 
 void displayPotentiometer(int valuePot) {
+    /*
+    Dedicated funciton for the menu corresponding to the digital potentiometer and its setting.
+    */
+
     ecranOLED.clearDisplay();
     for (int i = 0; i < sizeof(potentiometerItems) / sizeof(potentiometerItems[0]); i++) {
-        if (i == selectedItem) {
-        ecranOLED.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Highlight selected item
-        } else {
-        ecranOLED.setTextColor(SSD1306_WHITE); // Regular color for other items
-        }
+        ecranOLED.setTextColor(SSD1306_WHITE); // Regular color for the items
         ecranOLED.setCursor(0, i * 10); // Adjust position for each item
         ecranOLED.print(potentiometerItems[i]); // Print each element
     }
+    if (selectedItem != 0) {
+        ecranOLED.setCursor(0, ((sizeof(potentiometerItems) / sizeof(potentiometerItems[0])) - 1) * 10); // We set the cursor in the last element (back button)
+        ecranOLED.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Highlight the back button
+    }
+
     ecranOLED.display();
 }
 
